3c2dac60a9cd1c21366a8d781d78a574cbe934af
Sergio Garcia <sergiogarciagasco14@gmail.com>
2023-11-12 09:39:54 +0100

    Quote document and QuoteRepository creation

--- src/main/java/backend/projects/challengetui/entity/Quote.java
+++ src/main/java/backend/projects/challengetui/entity/Quote.java
@@ -1,4 +1,67 @@
 package backend.projects.challengetui.entity;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "quote")
 public class Quote {
+
+    @Id
+    private String id;
+
+    private String quoteText;
+    private String quoteAuthor;
+    private String quoteGenre;
+    private int __v;
+
+    public Quote() {
+    }
+
+    public Quote(String id, String quoteText, String quoteAuthor, String quoteGenre, int __v) {
+        this.id = id;
+        this.quoteText = quoteText;
+        this.quoteAuthor = quoteAuthor;
+        this.quoteGenre = quoteGenre;
+        this.__v = __v;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getQuoteText() {
+        return quoteText;
+    }
+
+    public void setQuoteText(String quoteText) {
+        this.quoteText = quoteText;
+    }
+
+    public String getQuoteAuthor() {
+        return quoteAuthor;
+    }
+
+    public void setQuoteAuthor(String quoteAuthor) {
+        this.quoteAuthor = quoteAuthor;
+    }
+
+    public String getQuoteGenre() {
+        return quoteGenre;
+    }
+
+    public void setQuoteGenre(String quoteGenre) {
+        this.quoteGenre = quoteGenre;
+    }
+
+    public int get__v() {
+        return __v;
+    }
+
+    public void set__v(int __v) {
+        this.__v = __v;
+    }
 }
--- src/main/java/backend/projects/challengetui/repository/QuoteRepository.java
+++ src/main/java/backend/projects/challengetui/repository/QuoteRepository.java
@@ -1,4 +1,12 @@
 package backend.projects.challengetui.repository;
 
-public class QuoteRepository {
+import backend.projects.challengetui.entity.Quote;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import java.util.List;
+
+public interface QuoteRepository extends MongoRepository<Quote, String> {
+
+    List<Quote> findByQuoteAuthor(String author);
+
 }
