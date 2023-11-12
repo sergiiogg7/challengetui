82b30535f991eb3707c1b9d03cefc5f3f6070323
Sergio Garcia <sergiogarciagasco14@gmail.com>
2023-11-12 09:19:15 +0100

    first commit

---
+++ src/main/java/backend/projects/challengetui/ChallengetuiApplication.java
@@ -0,0 +1,13 @@
+package backend.projects.challengetui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChallengetuiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChallengetuiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/backend/projects/challengetui/ChallengetuiApplicationTests.java
@@ -0,0 +1,13 @@
+package backend.projects.challengetui;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallengetuiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
